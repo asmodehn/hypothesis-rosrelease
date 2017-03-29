a750b7d5c1b66c6da82d06eca9722254966b021a
AlexV <asmodehn@gmail.com>
2017-03-29 15:23:31 +0900

    Imported upstream version '3.0.1' of 'upstream'

---
+++ tests/django/toystore/models.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+#
+# Most of this work is copyright (C) 2013-2015 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# https://github.com/DRMacIver/hypothesis/blob/master/CONTRIBUTING.rst for a
+# full list of people who may hold copyright, and consult the git log if you
+# need to determine who owns an individual contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, null=False)
+
+
+class CharmField(models.Field):
+
+    def db_type(self, connection):
+        return u'char(1)'
+
+
+class CustomishField(models.Field):
+
+    def db_type(self, connection):
+        return u'char(1)'
+
+
+class Customish(models.Model):
+    customish = CustomishField()
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    gender = models.CharField(max_length=50, null=True)
+    age = models.IntegerField()
+    birthday = models.DateTimeField()
+
+
+class Charming(models.Model):
+    charm = CharmField()
+
+
+class CouldBeCharming(models.Model):
+    charm = CharmField(null=True)
+
+
+class SelfLoop(models.Model):
+    me = models.ForeignKey(u'self', null=True)
+
+
+class LoopA(models.Model):
+    b = models.ForeignKey(u'LoopB', null=False)
+
+
+class LoopB(models.Model):
+    a = models.ForeignKey(u'LoopA', null=True)
+
+
+class ManyInts(models.Model):
+    i1 = models.IntegerField()
+    i2 = models.SmallIntegerField()
+    i3 = models.BigIntegerField()
+
+    p1 = models.PositiveIntegerField()
+    p2 = models.PositiveSmallIntegerField()
---
+++ tests/django/toystore/test_given_models.py
@@ -0,0 +1,83 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+#
+# Most of this work is copyright (C) 2013-2015 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# https://github.com/DRMacIver/hypothesis/blob/master/CONTRIBUTING.rst for a
+# full list of people who may hold copyright, and consult the git log if you
+# need to determine who owns an individual contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
+from hypothesis import given, assume
+from hypothesis.errors import InvalidArgument
+from hypothesis.strategies import just, lists
+from hypothesis.extra.django import TestCase, TransactionTestCase
+from tests.django.toystore.models import Store, Company, Customer, \
+    ManyInts, SelfLoop, Customish, CustomishField, CouldBeCharming
+from hypothesis.extra.django.models import models, \
+    add_default_field_mapping
+
+add_default_field_mapping(CustomishField, just(u'a'))
+
+
+class TestGetsBasicModels(TestCase):
+
+    @given(models(Company))
+    def test_is_company(self, company):
+        self.assertIsInstance(company, Company)
+        self.assertIsNotNone(company.pk)
+
+    @given(models(Store, company=models(Company)))
+    def test_can_get_a_store(self, store):
+        assert store.company.pk
+
+    @given(lists(models(Company)))
+    def test_can_get_multiple_models_with_unique_field(self, companies):
+        assume(len(companies) > 1)
+        for c in companies:
+            self.assertIsNotNone(c.pk)
+        self.assertEqual(
+            len({c.pk for c in companies}), len({c.name for c in companies})
+        )
+
+    @given(models(Customer))
+    def test_is_customer(self, customer):
+        self.assertIsInstance(customer, Customer)
+        self.assertIsNotNone(customer.pk)
+        self.assertIsNotNone(customer.email)
+
+    @given(models(CouldBeCharming))
+    def test_is_not_charming(self, not_charming):
+        self.assertIsInstance(not_charming, CouldBeCharming)
+        self.assertIsNotNone(not_charming.pk)
+        self.assertIsNone(not_charming.charm)
+
+    @given(models(SelfLoop))
+    def test_sl(self, sl):
+        self.assertIsNone(sl.me)
+
+    @given(lists(models(ManyInts)))
+    def test_no_overflow_in_integer(self, manyints):
+        pass
+
+    @given(models(Customish))
+    def test_custom_field(self, x):
+        assert x.customish == u'a'
+
+    def test_mandatory_fields_are_mandatory(self):
+        self.assertRaises(InvalidArgument, models, Store)
+
+
+class TestsNeedingRollback(TransactionTestCase):
+
+    def test_can_get_examples(self):
+        for _ in range(200):
+            models(Company).example()
